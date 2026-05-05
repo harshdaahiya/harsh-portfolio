390abe461ff49732ea9f643bc91bd646f2d13b7b
Harsh <onlybelieveharsh@gmail.com>
2026-05-05 10:54:21 +0530

    refactor: replace ExperienceCard grid with ExperienceList component in Experience section

--- src/components/landing/Experience.tsx
+++ src/components/landing/Experience.tsx
@@ -4,18 +4,14 @@ import React from 'react';
 
 import Container from '../common/Container';
 import SectionHeading from '../common/SectionHeading';
-import ExperienceCard from '../experience/ExperienceCard';
+import ExperienceList from '../experience/ExperienceList';
 import { Button } from '../ui/button';
 
 export default function Experience() {
   return (
     <Container>
       <SectionHeading subHeading="Experience" heading="Experience" />
-      <div className="">
-        {experiences.slice(0, 2).map((experience: Experience) => (
-          <ExperienceCard key={experience.company} experience={experience} />
-        ))}
-      </div>
+      <ExperienceList experiences={experiences} />
       <div className="mt-7 flex justify-center">
         <Button variant={'outline'} size="lg">
           <Link href="/experiece" className="text-foreground">
